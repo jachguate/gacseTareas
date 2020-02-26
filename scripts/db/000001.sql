bd4f720074f97c5b49451ea93298dbe43f8270f8
jachguate <jachguate@gmail.com>
2020-02-26 09:18:40 -0600

    Evolución del esquema antes de versión 1

--- scripts/db/000001.sql
+++ scripts/db/000001.sql
@@ -30,6 +30,7 @@ create table Tarea (
   , FechaLimite                     datetime
   , idResponsable                   bigint
   , idCategoria                     bigint
+  , FlagCompletada                  bit not null constraint DF_Tarea_FlagCompletada default (0)
   , btLogin                         varchar(50) not null constraint [DF_Tarea_btLogin]  default (suser_sname())
   , btFecha                         datetime not null constraint [DF_Tarea_btFecha]  default (getdate())
   , btHost                          varchar(50) not null constraint [DF_Tarea_btHost]  default (host_name())
@@ -46,3 +47,18 @@ create table Tarea (
       on update cascade
       on delete set null
 );
+
+if object_id('Control', 'U') is null
+create table Control (
+    PKControl                       char(1) not null constraint CK_Control_PKControl check (PKControl = 'X')
+  , VersionEstructura               int not null
+  , btLogin                         varchar(50) not null constraint [DF_Control_btLogin]  default (suser_sname())
+  , btFecha                         datetime not null constraint [DF_Control_btFecha]  default (getdate())
+  , btHost                          varchar(50) not null constraint [DF_Control_btHost]  default (host_name())
+  , constraint pkControl
+      primary key (PKControl)
+);
+
+
+if (select count(1) from Control) = 0
+insert into Control (PKControl, VersionEstructura) values ('X', 1);
